0661a20af3a3ee7d703cce13c46d1295efec4e65
PC-CASA-Edu <a@a.com>
2023-03-16 10:43:46 +0100

    Proyecto mas done que Alcapone

--- README.md
+++ README.md
@@ -29,3 +29,21 @@ Aqui almacenaremos la pagina web de la asignatura y ejemplos de varios lenguajes
  - [x] Aprender SQL
  
  Guion, espacio y llaves anguladas para una lista.
+
+ -¿Qué es HTML? ¿CSS? ¿Javascript?
+
+HTML, CSS y JavaScript son lenguajes que se utilizan para crear sitios web dinámicos e interactivos.
+
+HTML (Hypertext Markup Language) es el lenguaje de marcado utilizado para crear la estructura y contenido de las páginas web. Con HTML, se definen los elementos básicos de una página, como encabezados, párrafos, imágenes, enlaces, listas, tablas, etc.
+
+CSS (Cascading Style Sheets) es el lenguaje utilizado para diseñar la presentación y apariencia visual de las páginas web. Con CSS, se define cómo se deben mostrar los elementos HTML en la página, incluyendo el color, la fuente, el tamaño, la disposición, etc.
+
+JavaScript es un lenguaje de programación utilizado para crear interactividad y funcionalidad en las páginas web. Con JavaScript, se pueden crear efectos visuales, validar formularios, animar elementos, manipular el contenido de la página, interactuar con el usuario, y mucho más. Además, actualmente JavaScript se usa también en el lado servidor gracias a tecnologías como NodeJS.
+
+-¿Qué es el formato Markdown?
+
+Markdown es un lenguaje de marcado ligero utilizado para dar formato al texto en plataformas web, como foros, blogs y sitios web de documentación. Es el utilizado para archivos como README.md.
+
+-¿Qué es el formato JSON? ¿Cómo se convierte un objeto a JSON? ¿Y JSON a un objeto?
+
+JSON (JavaScript Object Notation) es un formato de intercambio de datos ligero utilizado para transmitir datos estructurados entre aplicaciones en la web. Para convertir un objeto a JSON en JavaScript, se utiliza el método JSON.stringify(). Para convertir una cadena JSON en un objeto JavaScript, se utiliza el método JSON.parse().
--- game.js
+++ game.js
@@ -1,117 +1,161 @@
 let game_data;
 		
-		let current_room = 0;
-		let items_picked = [];
+let current_room = 0;
+let items_picked = [];
+
+function game (data)
+{
+
+game_data = data;
+
+inventario = []
+
+document.getElementById("terminal").innerHTML = "<p><strong>¡Bienvenidos a ENTIerrame!</strong> Best juego de terror</p>";
+document.getElementById("terminal").innerHTML += "<p>Te encuentras en "+game_data.rooms[current_room].name+". ¿Que quieres hacer? </p>";
+}
+
+function terminal_out (info){
+	let terminal = document.getElementById("terminal");
+	terminal.innerHTML += info;
+	terminal.scrollTop = terminal.scrollHeight;
+}
+
+function mostrarItem(item){
+	terminal_out(`<p>Nombre: ${item.name}</p>`);
+	terminal_out(`<p>Descripcion: ${item.name}</p>`);
+}
+
+function parseInstruction (instruction){
+	switch (instruction[0]){
+	case "ver":
 		
-		function game (data)
-		{
+	break;
+	
+	case "ir":
 		
-		game_data = data;
-				
-		document.getElementById("terminal").innerHTML = "<p><strong>¡Bienvenidos a ENTIerrame!</strong> Best juego de terror</p>";
-		document.getElementById("terminal").innerHTML += "<p>Te encuentras en "+game_data.rooms[current_room].name+". ¿Que quieres hacer? </p>";
+		let connections = game_data.rooms[current_room].connection_door_room;
+		let doors = game_data.rooms[current_room].doors;
+		if(!doors.includes(instruction[1])){
+			terminal_out(`<p>Puerta errónea</p>`);
+		}else {
+			let doorIndex = doors.indexOf(instruction[1]);
+			let roomDestinationId = connections[doorIndex];
+			let destinationIndex = game_data.rooms.map((r)=>{return r.id}).indexOf(roomDestinationId);
+			let destination = game_data.rooms[destinationIndex];
+			current_room = destinationIndex;
+			console.log(destinationIndex)
+			terminal_out(`<p>Entrando en ${destination.name}</p>`);
+			terminal_out(`<p>${destination.description}</p>`);
 		}
 		
-		function terminal_out (info){
-			let terminal = document.getElementById("terminal");
-			terminal.innerHTML += info;
-			terminal.scrollTop = terminal.scrollHeight;
-		}
+	break;
+
+	case "inventario":
 		
-		function parseCommand (command){
-			console.log("El comando ", command);
-			switch (command){
-			case "ver":
-				terminal_out("<p>"game_data.rooms[current_room].description"</p>");
-				break;
-				
-			case "ir":
-				let doors = "";
-				let doors_num = game_data.rooms[current_room].doors.length;
-				
-				for (let i = 0; i < doors_num; i++){
-				doors += game_data.rooms[current_room].doors[i]+" ":
+		if(instruction.length == 1){
+			terminal_out(`<p>Contenido del inventario:</p>`);
+			if(inventario.length == 0){
+				terminal_out(`<p>Inventario vacío.</p>`);
+	
+			}else {
+				for (const item of inventario) {
+					mostrarItem(item);
+					terminal_out(`<p>-----</p>`);
+
 				}
-				terminal_out("<p>Puedes ir a: "+doors+"</p>");
-				break;
-					
-			default:
-				terminal_out("<p><strong>ERROR</strong>: "+command+" comando no encontrado</p>");
+	
+			}
+			
+		}else {
+			for (const item of inventario) {
+				if(item.id == instruction[1]){
+					mostrarItem(item);
+					break;
+				}
 			}
 		}
+
+
+	break;
+	
+	case "coger":
+		let currentRoom = game_data.rooms[current_room];
+		let selectedItem = instruction[1];
 		
-		function getRoomNumber (room){
-		for (let i = 0; i < game_data.rooms.length; i++){
+		if(currentRoom.items.includes(selectedItem)){
+			let selectedIndex = game_data.items.map((r)=>{return r.id}).indexOf(selectedItem);
+			let item = game_data.items[selectedIndex];
+			
+			if(item.pickable){
+				inventario.push(item);
+				terminal_out(`<p>${item.name} añadido al inventario.</p>`);
+				terminal_out(`<p>Descripcion: ${item.description}</p>`);
+			}else {
+				terminal_out(`<p>Parece que este item no se puede agarrar.</p>`);
+			}
+
+		}else {
+			terminal_out(`<p>No existe ese item en esta sala.</p>`);
+		}
+
+
+
+	break;
+	
+	default:
+		terminal_out("<p><strong>ERROR</strong>:")
+	}
+}
+
+function getRoomNumberById(roomId){
+	for (let i = 0; i < game_data.rooms.length; i++){
+		if (game_data.rooms[i].id == roomId){
+			return i;
+		}
+	}
+
+	return -1;
+}
+
+function getRoomNumber (room){
+	for (let i = 0; i < game_data.rooms.length; i++){
 		if (game_data.rooms[i].id == room){
-		return i;
-				}
-			}
-		
-		return -1;
+			return i;
 		}
-		
-		function getDoorNumber (door){
-		for (let i = 0; i < game_data.doors.length; i++){
-		if (game_data.doors[i].id == door){
-		return i;
-				}
-			}
-		
-		return -1;
+	}
+
+	return -1;
+}
+
+function getDoorNumber (door){
+	for (let i = 0; i < game_data.rooms[current_room].doors.length; i++){
+		if (game_data.rooms[current_room].doors[i].id == door){
+			return i;
 		}
-		
-		function parseInstruction (instruction){
-			console.log("La instrucción", instruction);
-			
-			switch (instruction[0]){
-			case "ver":
-				
-			break;
-			
-			case "ir":
-				
-				let door_num = getDoorNumber(instruction[1]);
-				if (door_num < 0) {
-				console.log("Puerta errónea");
-				return;
-				}
-				
-				let room_num = getRoomNumber(game_data.doors[door_num].rooms[0]);
-				if (room_num == current_room){
-				current_room = getRoomNumber(game_data.doors[door_num].rooms[1]);
-				}
-				else{
-				current_room= room_num;
-				}
-			break;
-			
-			case "coger":
-			
-			break;
-			
-			default:
-			terminal_out("<p><strong>ERROR</strong>:")
-			}
-		}
-		
-		function readAction(){
-		 let instruction = document.getElementById("commands").value;
-		 let instruction_trim instrucción.trim();
-		 
-		 let data = instruction.trim.split(" ");
-		
-		 if (data.length == 0 || instruction_trim == ""){
-		 document.getElementById("terminal").innerHTML += "<p><strong>ERROR</strong>: escribe una instrucción</p>";
-		 return;
-		 }
-		 
-		 if (data.length == 1){
-		   parseCommand(data[0]);
-		}
-		else{
-			parseInstruction(data);
-		}
-		
-		fetch("https://edsbru.github.io/game.json")
-		.then(response => response.json())
-		.then(data => game (data));
+	}
+
+	return -1;
+}
+
+
+function readAction(){
+	let instruction = document.getElementById("commands").value;
+	let instruction_trim = instruction.trim();
+	
+	let data = instruction_trim.split(" ");
+
+	if (data.length == 0 || instruction_trim == ""){
+		document.getElementById("terminal").innerHTML += "<p><strong>ERROR</strong>: escribe una instrucción</p>";
+		return;
+	}
+	// if (data.length == 1){
+	// 	parseCommand(data[0]);
+	// }
+	// else{
+	// }
+	parseInstruction(data);
+}
+
+fetch("https://edsbru.github.io/game.json")
+.then(response => response.json())
+.then(data => game (data));
